636cde5d480ad85d605ea323527a81dbe8882e1f
justin macklin <jmacklin308@gmail.com>
2021-04-25 01:38:37 -0500

    Added all the sprites

--- ShootingGallery/main.lua
+++ ShootingGallery/main.lua
@@ -9,6 +9,16 @@ function love.load()
     score = 0
     timer = 10 --seconds
 
+
+    --sprites
+    sprites = {}
+    sprites.background = love.graphics.newImage("res/sky.png")
+    sprites.target = love.graphics.newImage("res/target.png")
+    sprites.crosshairs = love.graphics.newImage("res/crosshairs.png")
+
+
+    ----turn off mouse cursor
+    love.mouse.setVisible(false)
 end
 
 
@@ -17,28 +27,32 @@ function love.update(dt)
     if timer > 0 then
         timer = timer - dt;
     end
-
+    --lock the timer at 0 once below
     if timer < 0 then
         timer = 0;
     end
 end
 
 function love.draw()
-    --draw the target
-    love.graphics.setColor(1, 0, 0, 1)
-    love.graphics.circle("fill", target.x, target.y, target.radius)
+    --draw background
+    love.graphics.draw(sprites.background,0,0)
 
     -- draw the score
     love.graphics.setColor(1, 1, 1, 1)
     love.graphics.setNewFont(40)
     love.graphics.print(score, 0,0)
 
-
     -- draw timer
     love.graphics.setColor(1, 1, 1, 1)
     love.graphics.setNewFont(40)
     love.graphics.print(math.ceil(timer),love.graphics.getWidth()/2,0)
 
+    ---- draw target
+    love.graphics.draw(sprites.target,target.x-target.radius,target.y-target.radius)
+
+
+    ----crosshairs
+    love.graphics.draw(sprites.crosshairs,love.mouse.getX()-20,love.mouse.getY()-20)
 end
 
 function love.mousepressed(x,y,button,istouch,presses)
